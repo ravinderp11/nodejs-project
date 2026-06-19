c29e65aa923d81ce74ee390642f413fd1b19984c
Ravinder <ravinder.pavusetty1989@gmail.com>
2026-06-19 17:00:17 +0000

    Fix ECR URL

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
+set -e
+
+AWS_REGION="us-east-1"
+ECR_URL="496097747127.dkr.ecr.us-east-1.amazonaws.com/nodejs-app"
+
+echo "Login to ECR"
+
 aws ecr get-login-password \
---region us-east-1 \
-| docker login \
+--region $AWS_REGION | \
+docker login \
 --username AWS \
 --password-stdin \
-YOUR_ECR_URL
+496097747127.dkr.ecr.us-east-1.amazonaws.com
 
+echo "Build Docker"
 
-docker build \
--t node-app \
--f app/Dockerfile \
-app
+docker build -t node-app ./app
 
+echo "Tag Docker"
 
-docker tag \
-node-app:latest \
-YOUR_ECR_URL:latest
+docker tag node-app:latest $ECR_URL:latest
 
+echo "Push Docker"
 
-docker push \
-YOUR_ECR_URL:latest
+docker push $ECR_URL:latest
